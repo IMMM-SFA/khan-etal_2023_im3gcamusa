7773d92702550578c0f57c25147856c1cdd5d3a5
Zarrar Khan <zarrar.khan@pnnl.gov>
2021-11-04 15:09:06 -0400

    Updating meta-repo

---
+++ R/khanetal2022im3gcamusa.R
@@ -0,0 +1,10 @@
+#' khanetal2022im3gcamusa
+#'
+#' \itemize{
+#' \item Github: https://github.com/IMMM-SFA/khan-etal_2022_im3gcamusa
+#' \item Webpage: https://khan-etal_2022_im3gcamusa.github.io/
+#' }
+#'
+#' @docType package
+#' @name khanetal2022im3gcamusa
+NULL
